05e2e9fa2d6fad71b978284da19e197c482785f7
felipeamp <felipeamp@gmail.com>
2017-04-19 12:17:31 -0300

    Improved monte carlo documentation and corrected clean_cache function to change global variables. Also corrected the starting points of both binary searches.

--- monte_carlo.py
+++ monte_carlo.py
@@ -26,7 +26,9 @@ _MONTE_CARLO_EXPECTED_CACHE = defaultdict(list)
 def clean_caches():
     """Cleans `_MONTE_CARLO_T_F_CACHE` and `_MONTE_CARLO_EXPECTED_CACHE`.
     """
+    global _MONTE_CARLO_T_F_CACHE
     _MONTE_CARLO_T_F_CACHE = {}
+    global _MONTE_CARLO_EXPECTED_CACHE
     _MONTE_CARLO_EXPECTED_CACHE = defaultdict(list)
 
 
@@ -72,49 +74,40 @@ def get_tests_and_fails_allowed(upper_p_value_threshold, lower_p_value_threshold
                         num_fails_allowed,
                         lower_p_value_threshold,
                         prob_monte_carlo):
+            # No value for num_tests will satisfy the inequality for lower_p_value_threshold for
+            # this value of num_fails_allowed.
             return None
 
+        # Exponential Search
         num_exp = 0
         num_tests_high = num_tests_ini + 1
+        num_tests_low = num_tests_ini
         while _is_ok_l(num_tests_high,
                        num_fails_allowed,
                        lower_p_value_threshold,
                        prob_monte_carlo):
+            num_tests_low = num_tests_high
             num_exp += 1
-            num_tests_high = num_tests_ini + 2**num_exp
-        num_tests_low = num_tests_high - 2**num_exp
+            num_tests_high += 2**num_exp
 
-        # # DEBUG:
-        # print('-'*80)
-        # print('num_fails_allowed =', num_fails_allowed)
-        # print('num_tests_low =', num_tests_low)
-        # print('num_tests_high =', num_tests_high)
-
+        # Binary Search
         while num_tests_high > num_tests_low:
+            # Loop Invariant:
+            # At least one of the bounds (low or high) will satisfy the inequality for
+            # lower_p_value_threshold.
+
+            # Base case:
+            # When num_tests_high == num_tests_low + 1, the midpoint will be num_tests_high. If it
+            # satisfies the inequality, that's our answer. Otherwise, it is num_tests_low.
             num_tests_mid = (num_tests_low + num_tests_high + 1) // 2 # Round up
             if _is_ok_l(num_tests_mid,
                         num_fails_allowed,
                         lower_p_value_threshold,
                         prob_monte_carlo):
                 num_tests_low = num_tests_mid
-
-                # # DEBUG:
-                # print('num_tests_low =', num_tests_low)
-                # print('num_tests_high =', num_tests_high)
-
             else:
                 num_tests_high = num_tests_mid - 1
 
-                # # DEBUG:
-                # print('num_tests_low =', num_tests_low)
-                # print('num_tests_high =', num_tests_high)
-
-        # Maybe we have num_tests_low == num_tests_high but it doesn't satisfies the condition for L
-        if not _is_ok_l(num_tests_high,
-                        num_fails_allowed,
-                        lower_p_value_threshold,
-                        prob_monte_carlo):
-            return None
         return num_tests_high
 
     def _find_smallest_num_tests_u(num_fails_allowed, upper_p_value_threshold, prob_monte_carlo):
@@ -129,68 +122,55 @@ def get_tests_and_fails_allowed(upper_p_value_threshold, lower_p_value_threshold
                     upper_p_value_threshold,
                     prob_monte_carlo,
                     num_valid_nominal_attributes):
+            # If num_tests_ini satisfies the inequality, that's our answer.
             return num_tests_ini
 
+        # Exponential Search
         num_exp = 0
         num_tests_high = num_tests_ini + 1
+        num_tests_low = num_tests_ini
         while not _is_ok_u(num_tests_high,
                            num_fails_allowed,
                            upper_p_value_threshold,
                            prob_monte_carlo,
                            num_valid_nominal_attributes):
+            num_tests_low = num_tests_high
             num_exp += 1
-            num_tests_high = num_tests_ini + 2**num_exp
-        num_tests_low = num_tests_high - 2**num_exp
+            num_tests_high += 2**num_exp
 
-        # # DEBUG:
-        # print('-'*80)
-        # print('num_fails_allowed =', num_fails_allowed)
-        # print('num_tests_low =', num_tests_low)
-        # print('num_tests_high =', num_tests_high)
-
+        # Binary Search
         while num_tests_high > num_tests_low:
-            num_tests_mid = (num_tests_low + num_tests_high) // 2
+            # Loop Invariant:
+            # At least one of the bounds (low or high) will satisfy the inequality for
+            # upper_p_value_threshold.
+
+            # Base case:
+            # When num_tests_high == num_tests_low + 1, the midpoint will be num_tests_low. If it
+            # satisfies the inequality, that's our answer. Otherwise, it is num_tests_high.
+            num_tests_mid = (num_tests_low + num_tests_high) // 2 # Round down.
             if _is_ok_u(num_tests_mid,
                         num_fails_allowed,
                         upper_p_value_threshold,
                         prob_monte_carlo,
                         num_valid_nominal_attributes):
                 num_tests_high = num_tests_mid
-
-                # # DEBUG:
-                # print('num_tests_low =', num_tests_low)
-                # print('num_tests_high =', num_tests_high)
-
             else:
                 num_tests_low = num_tests_mid + 1
 
-                # # DEBUG:
-                # print('num_tests_low =', num_tests_low)
-                # print('num_tests_high =', num_tests_high)
-
-        # Maybe we have num_tests_low == num_tests_high but it doesn't satisfies the condition for U
-        if not _is_ok_u(num_tests_high,
-                        num_fails_allowed,
-                        upper_p_value_threshold,
-                        prob_monte_carlo,
-                        num_valid_nominal_attributes):
-            return None
         return num_tests_high
 
-    # # DEBUG:
-    # print('Starting get_tests_and_fails_allowed...')
+
     assert upper_p_value_threshold > lower_p_value_threshold
     assert upper_p_value_threshold <= 1.0 and upper_p_value_threshold > 0.0
     assert lower_p_value_threshold < 1.0 and lower_p_value_threshold >= 0.0
     assert prob_monte_carlo >= 0.0 and prob_monte_carlo <= 1.0
     assert num_valid_nominal_attributes > 0
 
+    # Cache Lookup
     if (upper_p_value_threshold,
             lower_p_value_threshold,
             prob_monte_carlo,
             num_valid_nominal_attributes) in _MONTE_CARLO_T_F_CACHE:
-        # # DEBUG:
-        # print('Found in cache!')
         return _MONTE_CARLO_T_F_CACHE[(upper_p_value_threshold,
                                        lower_p_value_threshold,
                                        prob_monte_carlo,
@@ -199,14 +179,9 @@ def get_tests_and_fails_allowed(upper_p_value_threshold, lower_p_value_threshold
     # We need to calculate these new values of num_tests and num_fails_allowed.
     num_fails_allowed = 0
     while True:
-        # # DEBUG:
-        # print('-----')
-        # print('num_fails_allowed:', num_fails_allowed)
         largest_num_tests_l = _find_largest_num_tests_l(num_fails_allowed,
                                                         lower_p_value_threshold,
                                                         prob_monte_carlo)
-        # # DEBUG:
-        # print('largest_num_tests_l:', largest_num_tests_l)
         if largest_num_tests_l is None:
             num_fails_allowed += 1
             continue
@@ -214,16 +189,12 @@ def get_tests_and_fails_allowed(upper_p_value_threshold, lower_p_value_threshold
         smallest_num_tests_u = _find_smallest_num_tests_u(num_fails_allowed,
                                                           upper_p_value_threshold,
                                                           prob_monte_carlo)
-        # # DEBUG:
-        # print('smallest_num_tests_u:', smallest_num_tests_u)
         if smallest_num_tests_u is None:
             num_fails_allowed += 1
             continue
 
         if smallest_num_tests_u <= largest_num_tests_l:
-            # # DEBUG:
-            # print('found!')
-            # print('t =', smallest_num_tests_u)
+            # Found the answer! Save in cache and return it.
             _MONTE_CARLO_T_F_CACHE[(upper_p_value_threshold,
                                     lower_p_value_threshold,
                                     prob_monte_carlo,
@@ -231,6 +202,7 @@ def get_tests_and_fails_allowed(upper_p_value_threshold, lower_p_value_threshold
                                                                       num_fails_allowed)
             return (smallest_num_tests_u, num_fails_allowed)
         num_fails_allowed += 1
+
     return (None, None)
 
 
@@ -275,49 +247,18 @@ def get_expected_total_num_tests(num_tests, num_fails_allowed, num_valid_nominal
     else:
         o_i = _MONTE_CARLO_EXPECTED_CACHE[(num_tests, num_fails_allowed)][-1]
 
-        # DEBUG:
-        # print('o_1 =', o_i)
 
-    # `i` is the current number of valid nominal attributes
-    for i in range(starting_num_attributes + 1, num_valid_nominal_attributes + 1):
-
-        # # DEBUG:
-        # print('='*80)
-        # print('i =', i)
-
+    for _ in range(starting_num_attributes + 1, num_valid_nominal_attributes + 1):
         r_i_prev = _get_r_i(num_tests,
                             num_fails_allowed,
                             s_array,
                             ds_array,
                             o_i,
                             max_value_for_root)
-
-        # # DEBUG
-        # print('r_{i-1}:')
-        # print(r_i_prev)
-        # print('*'*50)
-        # print('Calculating possible increments:')
-
         max_increment_found = max(_get_increment_o_i(root, num_tests, num_fails_allowed, o_i)
                                   for root in r_i_prev)
         o_i += max_increment_found
-
-        # # DEBUG:
-        # print('*'*50)
-        # print('max_increment_found =', max_increment_found)
-        # print('o_i =', o_i)
-
         _MONTE_CARLO_EXPECTED_CACHE[(num_tests, num_fails_allowed)].append(o_i)
-
-    # # DEBUG
-    # print('='*80)
-    # print('num_tests =', num_tests)
-    # print('num_fails_allowed =', num_fails_allowed)
-    # print('num_valid_nominal_attributes =', num_valid_nominal_attributes)
-    # print('o_m =', o_i)
-    # print('E =', o_i)
-    # print('E/num_valid_nominal_attributes =', o_i / num_valid_nominal_attributes)
-    # print('='*80)
 
     return o_i
 
@@ -365,28 +306,11 @@ def _get_r_i(num_tests, num_fails_allowed, s_array, ds_array, o_i, max_value_for
     roots.add(0.0)
     roots.add(max_value_for_root)
 
-    # # DEBUG
-    # if len(roots) > 2:
-    #     print('==*=='*20)
-    #     print('\t\tFound {} roots!'.format(len(roots) - 2))
-    #     print('\t\tIncluding the interval boundaries, we have')
-    #     print('\t\troots =', roots)
-    #     print('==*=='*20)
-
     return roots
 
 
 def _get_poly_roots_in_interval(coef_array, min_value_for_root, max_value_for_root):
-    # # DEBUG
-    # print('coef_array:')
-    # print(coef_array)
-
     roots = np.roots(coef_array)
-
-    # # DEBUG
-    # print('roots:')
-    # print(roots)
-
     ret = set()
     for is_real, root in zip(np.isreal(roots), roots):
         if is_real and root.real >= min_value_for_root and root.real <= max_value_for_root:
@@ -398,18 +322,8 @@ def _get_increment_o_i(root, num_tests, num_fails_allowed, o_i):
     s = _get_s(root, num_tests, num_fails_allowed)
     ret = ((num_fails_allowed + 1) / (1. - root)
            + s * (num_tests - o_i - num_fails_allowed - 1) / (1. - root))
-
-    # # DEBUG
-    # print('-'*40)
-    # print('root =', root)
-    # print('num_tests =', num_tests)
-    # print('num_fails_allowed =', num_fails_allowed)
-    # print('o_{i-1} =', o_i)
-    # print('s =', s)
-    # print('increment_o_i =', ret)
-
     return ret
 
 
-def _get_s(prob, num_tests, num_fails_allowed):
-    return binom_stats.cdf(num_fails_allowed, num_tests, 1. - prob)
+def _get_s(prob_success, num_tests, num_fails_allowed):
+    return binom_stats.cdf(num_fails_allowed, num_tests, 1. - prob_success)

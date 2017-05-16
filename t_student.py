f66b6e0d683de06e8d15b80ccdcf52a5baec7766
felipeamp <felipeamp@gmail.com>
2017-05-16 17:29:52 -0300

    Updated and refactored t_student.py.

--- t_student.py
+++ t_student.py
@@ -17,47 +17,27 @@ from scipy.stats import t as student_t
 
 ColumnIndices = collections.namedtuple('ColumnIndices',
                                        ['dataset_col',
-                                        'attribute_col',
-                                        'num_values_col',
                                         'criterion_col',
                                         'trial_number_col',
-                                        'fold_number_col',
                                         'accuracy_w_missing_col',
                                         'accuracy_wo_missing_col',
                                         'num_nodes_col'])
 
-#: Contain the column indices for a rank experiment
-RANK_COLUMN_INDICES = ColumnIndices(dataset_col=1,
-                                    attribute_col=3,
-                                    num_values_col=5,
-                                    criterion_col=8,
-                                    trial_number_col=7,
-                                    fold_number_col=11,
-                                    accuracy_w_missing_col=29,
-                                    accuracy_wo_missing_col=30,
-                                    num_nodes_col=33)
-
-#: Contain the column indices for a cross-validation experiment
+#: Contain the column indices for a cross-validation experiment. Starts at zero.
 CROSS_VALIDATION_COLUMN_INDICES = ColumnIndices(dataset_col=1,
-                                                attribute_col=None,
-                                                num_values_col=None,
                                                 criterion_col=4,
                                                 trial_number_col=3,
-                                                fold_number_col=None,
-                                                accuracy_w_missing_col=20,
-                                                accuracy_wo_missing_col=21,
-                                                num_nodes_col=28)
+                                                accuracy_w_missing_col=22,
+                                                accuracy_wo_missing_col=23,
+                                                num_nodes_col=30)
 
-#: Contain the column indices for a train-and-test experiment
+#: Contain the column indices for a train-and-test experiment. Starts at zero.
 TRAIN_AND_TEST_COLUMN_INDICES = ColumnIndices(dataset_col=1,
-                                              attribute_col=None,
-                                              num_values_col=None,
-                                              criterion_col=7,
+                                              criterion_col=6,
                                               trial_number_col=5,
-                                              fold_number_col=None,
-                                              accuracy_w_missing_col=20,
-                                              accuracy_wo_missing_col=21,
-                                              num_nodes_col=24)
+                                              accuracy_w_missing_col=33,
+                                              accuracy_wo_missing_col=34,
+                                              num_nodes_col=37)
 
 
 def main(output_path):
@@ -79,29 +59,20 @@ def main(output_path):
         sys.exit(1)
     with open(experiment_config_filepath, 'r') as experiment_config_json:
         experiment_config = json.load(experiment_config_json)
-    if "min num values to compare" in experiment_config:
-        min_num_values_to_compare = experiment_config["min num values to compare"]
-    else:
-        min_num_values_to_compare = 2
 
-    if experiment_config["rank attributes"]:
-        is_rank = True
-        column_indices = RANK_COLUMN_INDICES
-    elif experiment_config["use cross-validation"]:
-        is_rank = False
+    if experiment_config["use cross-validation"]:
         column_indices = CROSS_VALIDATION_COLUMN_INDICES
     else:
-        is_rank = False
         column_indices = TRAIN_AND_TEST_COLUMN_INDICES
 
     single_sided_p_value_threshold = experiment_config["t-test single-sided p-value"]
 
-    raw_data = _load_raw_data(raw_output_path, column_indices, is_rank, min_num_values_to_compare)
-    _save_raw_stats(raw_data, output_path, is_rank)
+    raw_data = _load_raw_data(raw_output_path, column_indices)
+    _save_raw_stats(raw_data, output_path)
     _save_aggreg_stats(output_path, single_sided_p_value_threshold)
 
 
-def _load_raw_data(raw_output_path, column_indices, is_rank, min_num_values_to_compare=2):
+def _load_raw_data(raw_output_path, column_indices):
     def _init_raw_data():
         # This function creates (in a lazy way) an infinitely-nested default dict. This is
         # useful when creating a default dict highly nested.
@@ -127,30 +98,13 @@ def _load_raw_data(raw_output_path, column_indices, is_rank, min_num_values_to_c
                 accuracy_wo_missing = None
             num_nodes = float(line_list[column_indices.num_nodes_col])
 
-            if is_rank:
-                try:
-                    num_values = int(line_list[column_indices.num_values_col])
-                    if num_values < min_num_values_to_compare:
-                        continue
-                except ValueError:
-                    # Numeric attribute
-                    if min_num_values_to_compare > 2:
-                        # In this case we assume we are only interested in nominal attributes.
-                        continue
-                attribute_name = line_list[column_indices.attribute_col]
-                fold_number = line_list[column_indices.fold_number_col]
-                raw_data[dataset_name][attribute_name][criterion_name][trial_number][
-                    fold_number] = (accuracy_w_missing,
-                                    accuracy_wo_missing,
-                                    num_nodes)
-            else:
-                raw_data[dataset_name][criterion_name][trial_number] = (accuracy_w_missing,
-                                                                        accuracy_wo_missing,
-                                                                        num_nodes)
+            raw_data[dataset_name][criterion_name][trial_number] = (accuracy_w_missing,
+                                                                    accuracy_wo_missing,
+                                                                    num_nodes)
     return raw_data
 
 
-def _save_raw_stats(raw_data, output_path, is_rank):
+def _save_raw_stats(raw_data, output_path):
     raw_stats_output_file = os.path.join(output_path, 'raw_t_student_stats.csv')
     with open(raw_stats_output_file, 'w') as fout:
         header = ['Dataset',
@@ -166,107 +120,51 @@ def _save_raw_stats(raw_data, output_path, is_rank):
                   'Degrees of Freedom of Number of Nodes',
                   'P-value t-statistics on Number of Nodes']
         print(','.join(header), file=fout)
-        if is_rank:
-            for dataset_name in raw_data:
-                for attribute_name in raw_data[dataset_name]:
-                    for (criterion_name_1,
-                         criterion_name_2) in itertools.combinations(
-                             raw_data[dataset_name][attribute_name], 2):
 
+        for dataset_name in raw_data:
+            for (criterion_name_1,
+                 criterion_name_2) in itertools.combinations(raw_data[dataset_name], 2):
 
-                        criterion_diff_name = ' - '.join((criterion_name_1, criterion_name_2))
-                        accuracy_w_missing_diff = []
-                        accuracy_wo_missing_diff = []
-                        num_nodes_diff = []
+                criterion_diff_name = ' - '.join((criterion_name_1, criterion_name_2))
+                accuracy_w_missing_diff = []
+                accuracy_wo_missing_diff = []
+                num_nodes_diff = []
 
-                        trial_number_intersection = (
-                            set(raw_data[dataset_name][attribute_name][criterion_name_1].keys())
-                            & set(raw_data[dataset_name][attribute_name][criterion_name_2].keys()))
-                        for trial_number in trial_number_intersection:
-                            fold_number_intersection = (
-                                set(raw_data[dataset_name][attribute_name][criterion_name_1][
-                                    trial_number].keys())
-                                & set(raw_data[dataset_name][attribute_name][criterion_name_2][
-                                    trial_number].keys()))
-                            for fold_number in fold_number_intersection:
-                                criterion_1_data = raw_data[dataset_name][attribute_name][
-                                    criterion_name_1][trial_number][fold_number]
-                                criterion_2_data = raw_data[dataset_name][attribute_name][
-                                    criterion_name_2][trial_number][fold_number]
+                trial_number_intersection = (
+                    set(raw_data[dataset_name][criterion_name_1].keys())
+                    & set(raw_data[dataset_name][criterion_name_2].keys()))
+                for trial_number in trial_number_intersection:
+                    criterion_1_data = raw_data[dataset_name][criterion_name_1][trial_number]
+                    criterion_2_data = raw_data[dataset_name][criterion_name_2][trial_number]
 
-                                accuracy_w_missing_diff.append(
-                                    criterion_1_data[0] - criterion_2_data[0])
-                                if (criterion_1_data[1] is not None
-                                        and criterion_2_data[1] is not None):
-                                    accuracy_wo_missing_diff.append(
-                                        criterion_1_data[1] - criterion_2_data[1])
-                                num_nodes_diff.append(
-                                    criterion_1_data[2] - criterion_2_data[2])
+                    accuracy_w_missing_diff.append(
+                        criterion_1_data[0] - criterion_2_data[0])
+                    if (criterion_1_data[1] is not None
+                            and criterion_2_data[1] is not None):
+                        accuracy_wo_missing_diff.append(
+                            criterion_1_data[1] - criterion_2_data[1])
+                    num_nodes_diff.append(
+                        criterion_1_data[2] - criterion_2_data[2])
 
-                        (t_statistic_w_missing,
-                         p_value_w_missing) = _calculate_t_statistic(accuracy_w_missing_diff)
-                        (t_statistic_wo_missing,
-                         p_value_wo_missing) = _calculate_t_statistic(accuracy_wo_missing_diff)
-                        (t_statistic_num_nodes,
-                         p_value_num_nodes) = _calculate_t_statistic(num_nodes_diff)
-                        print(','.join([dataset_name,
-                                        attribute_name,
-                                        criterion_diff_name,
-                                        str(t_statistic_w_missing),
-                                        str(len(accuracy_w_missing_diff) - 1),
-                                        str(p_value_w_missing),
-                                        str(t_statistic_wo_missing),
-                                        str(len(accuracy_wo_missing_diff) - 1),
-                                        str(p_value_wo_missing),
-                                        str(t_statistic_num_nodes),
-                                        str(len(num_nodes_diff) - 1),
-                                        str(p_value_num_nodes)]),
-                              file=fout)
-        else:
-            for dataset_name in raw_data:
-                for (criterion_name_1,
-                     criterion_name_2) in itertools.combinations(raw_data[dataset_name], 2):
-
-                    criterion_diff_name = ' - '.join((criterion_name_1, criterion_name_2))
-                    accuracy_w_missing_diff = []
-                    accuracy_wo_missing_diff = []
-                    num_nodes_diff = []
-
-                    trial_number_intersection = (
-                        set(raw_data[dataset_name][criterion_name_1].keys())
-                        & set(raw_data[dataset_name][criterion_name_2].keys()))
-                    for trial_number in trial_number_intersection:
-                        criterion_1_data = raw_data[dataset_name][criterion_name_1][trial_number]
-                        criterion_2_data = raw_data[dataset_name][criterion_name_2][trial_number]
-
-                        accuracy_w_missing_diff.append(
-                            criterion_1_data[0] - criterion_2_data[0])
-                        if (criterion_1_data[1] is not None
-                                and criterion_2_data[1] is not None):
-                            accuracy_wo_missing_diff.append(
-                                criterion_1_data[1] - criterion_2_data[1])
-                        num_nodes_diff.append(
-                            criterion_1_data[2] - criterion_2_data[2])
-
-                    (t_statistic_w_missing,
-                     p_value_w_missing) = _calculate_t_statistic(accuracy_w_missing_diff)
-                    (t_statistic_wo_missing,
-                     p_value_wo_missing) = _calculate_t_statistic(accuracy_wo_missing_diff)
-                    (t_statistic_num_nodes,
-                     p_value_num_nodes) = _calculate_t_statistic(num_nodes_diff)
-                    print(','.join([dataset_name,
-                                    str(None),
-                                    criterion_diff_name,
-                                    str(t_statistic_w_missing),
-                                    str(len(accuracy_w_missing_diff) - 1),
-                                    str(p_value_w_missing),
-                                    str(t_statistic_wo_missing),
-                                    str(len(accuracy_wo_missing_diff) - 1),
-                                    str(p_value_wo_missing),
-                                    str(t_statistic_num_nodes),
-                                    str(len(num_nodes_diff) - 1),
-                                    str(p_value_num_nodes)]),
-                          file=fout)
+                (t_statistic_w_missing,
+                 p_value_w_missing) = _calculate_t_statistic(accuracy_w_missing_diff)
+                (t_statistic_wo_missing,
+                 p_value_wo_missing) = _calculate_t_statistic(accuracy_wo_missing_diff)
+                (t_statistic_num_nodes,
+                 p_value_num_nodes) = _calculate_t_statistic(num_nodes_diff)
+                print(','.join([dataset_name,
+                                str(None),
+                                criterion_diff_name,
+                                str(t_statistic_w_missing),
+                                str(len(accuracy_w_missing_diff) - 1),
+                                str(p_value_w_missing),
+                                str(t_statistic_wo_missing),
+                                str(len(accuracy_wo_missing_diff) - 1),
+                                str(p_value_wo_missing),
+                                str(t_statistic_num_nodes),
+                                str(len(num_nodes_diff) - 1),
+                                str(p_value_num_nodes)]),
+                      file=fout)
 
 
 def _calculate_t_statistic(samples_list):
